c28a74e9cd4ffff8736f3492b49f86fd6d2ec5b0
Leoj030 <refuerzojoel1@gmail.com>
2026-03-29 11:18:15 +0800

    ref: change matcher of proxy to only work on protected routes

--- src/proxy.js
+++ src/proxy.js
@@ -38,6 +38,10 @@ export async function proxy(request) {
 
 export const config = {
     matcher: [
-        "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+        '/me/:path*',
+        '/dept/:path*',
+        '/admin/:path*',
+        '/hr/:path*',
+        '/finance/:path*'
     ],
 };
